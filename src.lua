a441d9011b5647d05e0ae543b1489dd1bc00c042
GTimbly <87491137+GTimbly@users.noreply.github.com>
2022-08-22 11:59:25 -0400

    Update src.lua

--- src.lua
+++ src.lua
@@ -689,7 +689,7 @@ function Library:create(options)
 		BackgroundTransparency = 1,
 		Position = UDim2.new(0, 5, 0.5, 0),
 		Size = UDim2.new(0, 15, 0, 15),
-		Image = "http://www.roblox.com/asset/?id=10667571401",
+		Image = "rbxassetid://7733936115",
 		Theme = {ImageColor3 = "StrongText"}
 	})
 
