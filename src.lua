f408e4e2c85fd3cfa90d85f4621909e55bec4fa5
GTimbly <87491137+GTimbly@users.noreply.github.com>
2022-08-22 11:50:17 -0400

    Update src.lua

--- src.lua
+++ src.lua
@@ -50,7 +50,7 @@ local Library = {
 		Dark = {
 			Main = Color3.fromRGB(30, 30, 30),
 			Secondary = Color3.fromRGB(50, 50, 50),
-			Tertiary = Color3.fromRGB(163, 0, 0),
+			Tertiary = Color3.fromRGB(255, 0, 0),
 
 			StrongText = Color3.fromHSV(0, 0, 1),		
 			WeakText = Color3.fromHSV(0, 0, 172/255)
